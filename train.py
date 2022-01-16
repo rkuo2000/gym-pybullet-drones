82434766db7ff3e9f195a4b8c53e0d9a819137bb
Richard Kuo <rkuo2000@gmail.com>
2022-01-16 10:23:01 +0800

    Add files via upload

---
+++ train.py
@@ -0,0 +1,17 @@
+import gym
+from stable_baselines3 import A2C
+from stable_baselines3.common.env_checker import check_env
+
+#### Check the environment's spaces ########################
+env = gym.make("takeoff-aviary-v0")      # 起飛
+#env = gym.make("hover-aviary-v0")       # 旋停
+#env = gym.make("flythrugate-aviary-v0") # 穿越
+
+print("[INFO] Action space:", env.action_space)
+print("[INFO] Observation space:", env.observation_space)
+check_env(env, warn=True, skip_render_check=True)
+
+model = A2C('MlpPolicy', env, verbose=1)
+model.learn(total_timesteps=10000)
+model.save('drone_a2c')
+
